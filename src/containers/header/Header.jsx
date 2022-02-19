34b257ff4be2e91d703ebf45fa28d4fc58249ca1
alireza <alireza.ramezani@weknowit.se>
2022-02-19 16:27:15 +0100

    Completed the navbar

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,19 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id="Home">
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's build our first react app.
+
+          <div className='gpt3__header-content__input'>
+             <input type="email" placeholder='Your email adress'>
+               
+             </input>
+          </div>
+        </h1>
+      </div>
+    </div>
   )
 }
 
