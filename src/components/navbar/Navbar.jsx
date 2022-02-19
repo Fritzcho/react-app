e9972178fced1c502e620d34e8d7ad3c9a421292
alireza <alireza.ramezani@weknowit.se>
2022-02-19 13:48:19 +0100

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloseLin } from 'react-icons-ri';
+
+
+const Navbar = () => {
+  return (
+    <div className="gptr__navbar">
+      <div className="gpt3__navbar-links">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
